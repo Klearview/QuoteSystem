3fb4a289a026569fdeb70262f257aa8beb753e18
Noah Tomkins <1tomkinsnoa@gmail.com>
2022-09-29 18:12:44 -0400

    Special Charcters removed from search

--- KlearviewQuotes/Models/CustomerInfo.cs
+++ KlearviewQuotes/Models/CustomerInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 
 namespace KlearviewQuotes.Models
@@ -30,17 +31,25 @@ namespace KlearviewQuotes.Models
 
         public bool Contains(string search)
         {
-            search = search.ToLower();
+            search = FormatString(search);
 
             return (
-                (Name != null && Name.ToLower().Contains(search))||
-                (AccountNumber != null && AccountNumber.ToLower().Contains(search)) ||
-                (Address != null && Address.ToLower().Contains(search)) ||
-                (Phone != null && Phone.ToLower().Contains(search)) ||
-                (Cell != null && Cell.ToLower().Contains(search)) ||
-                (Work != null && Work.ToLower().Contains(search)) ||
-                (Email != null && Email.ToLower().Contains(search))
+                (Name != null && FormatString(Name).Contains(search))||
+                (AccountNumber != null && FormatString(AccountNumber).Contains(search)) ||
+                (Address != null && FormatString(Address).Contains(search)) ||
+                (Phone != null && FormatString(Phone).Contains(search)) ||
+                (Cell != null && FormatString(Cell).Contains(search)) ||
+                (Work != null && FormatString(Work).Contains(search)) ||
+                (Email != null && FormatString(Email).Contains(search))
             );
+        }
+
+        private string FormatString(string input)
+        {
+            input = input.ToLower();
+
+            string regExp = "\\W";
+            return Regex.Replace(input, regExp, "");
         }
     }
 }
